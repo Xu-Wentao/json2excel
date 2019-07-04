8435ed3049949619580cd564c92e8b9beed73d32
徐文涛 <cutenear1993@gmail.com>
2019-07-04 15:48:22 +0800

    add file

---
+++ make_excel_file.py
@@ -0,0 +1,37 @@
+# -*- coding: UTF-8 -*-
+
+import openpyxl
+
+from chongzhi.common.logging import INTERNAL_LOG as LOG
+
+
+def make_excel_file(file_name, sheets: list):
+    """
+    :param file_name: excel name you wanna create
+    :param sheets: for example
+     [{'title':'sheet0',
+       'headers':{'name':'姓名','age':'年龄'},
+       'data_list':[
+            {'name':'admin','age':12},
+            {'name':'guest','age':12},
+       ]}]
+    """
+    wb = openpyxl.Workbook()
+    try:
+        for index, sheet in enumerate(sheets):
+            if index == 0:
+                ws = wb.worksheets[0]
+                ws.title = sheet['title']
+            else:
+                ws = wb.create_sheet(sheet['title'])
+            headers = sheet['headers']
+            for i, value in enumerate(headers.values()):
+                ws.cell(row=1, column=i + 1, value=value)
+            for i, data in enumerate(sheet['data_list']):
+                for j, key in enumerate(headers.keys()):
+                    ws.cell(row=i + 2, column=j + 1, value=data[key])
+        wb.save(filename=file_name)
+        return file_name
+    except Exception as e:
+        LOG.error(e, exc_info=True)
+        raise
